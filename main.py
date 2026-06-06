6fcb388917d166dd368adfacaf89a358d00408bf
SupercoolPayloadGuy <techandcodetester@gmail.com>
2026-06-06 07:56:33 +0200

    fixed bug

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ from animation    import RestartSignal
 
 def parse_args():
     p = argparse.ArgumentParser(description="Circle Construction")
-    p.add_argument("-p", "--points", type=int, default=4,
+    p.add_argument("-p", "--points", type=int, default=0,
                    help="Number of base points (overrides intro selection)")
     return p.parse_args()
 
